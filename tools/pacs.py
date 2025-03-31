fc96104214604218a5a94af3378f9a306031298e
Michał Fita <4925040+michalfita@users.noreply.github.com>
2025-03-31 19:35:21 +0100

    Chore(release): v0.33.5

--- tools/pacs.py
+++ tools/pacs.py
@@ -54,7 +54,7 @@ def main(argv: list[str]):
         data = {
             'crate': str(pac_name),
             'chip': str(pac_name).upper(),
-            'svd2rust_version': "0.30.3",  # TODO: keep in synced
+            'svd2rust_version': "0.33.5",  # TODO: keep in synced
             'atpack_version': svd_version_map.get(str(pac_name).upper() + '.svd', "(unknown)"),
         }
         link_svd_file(pathlib.Path(os.getcwd(), args.svddir, svd_file), pathlib.Path(os.getcwd(), pac_path))
